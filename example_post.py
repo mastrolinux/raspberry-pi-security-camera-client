d8e4765baf84a0c8c2240ace25603c06dce0da7e
umbynos <umberto.baldi@edu.unito.it>
2022-04-15 16:29:58 +0200

    change filetype, test with actual name, and print more info

--- example_post.py
+++ example_post.py
@@ -1,7 +1,8 @@
 import requests
 
 url = 'https://camera-server.onrender.com/upload'
-files = {'media': open('/tmp/test.jpg', 'rb')}
+files = {'file': open('/tmp/2022-04-10T20:57:06.706411.jpg', 'rb')}
 r = requests.post(url, files=files)
 
-print (r.status_code)
+print (r.json())
+r.raise_for_status()
